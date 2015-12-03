3046e79d601001cde8ed8d9b003595495d4ac38c
Bellov <belovsallvatore@gmail.com>
2015-12-03 20:14:24 +0200

    Intro-Programming-Homework

---
+++ 08.CurrentDateAndTime/DateAndTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+//Create a console application that prints the current date and time. Find in Internet how.
+
+class DateAndTime
+{
+    static void Main()
+    {
+        Console.WriteLine(DateTime.Now);
+    }
+
+}
+
---
+++ 09.AgeAfter10Years/AgeAfter10Years.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+
+class AgeAfter10Years
+{
+    static void Main()
+    {
+        DateTime today = DateTime.Now;
+        Console.WriteLine("Your birthday is (please follow the format yyyy,mm,dd); ");
+        DateTime date = DateTime.Parse(Console.ReadLine());
+        int age = today.Year - date.Year;
+        if (today < date.AddYears(age)) age--;
+        Console.WriteLine("You are now {0} years old.",age);
+        Console.WriteLine("In 10 years you will be {0} years old.",age+10);
+
+    }
+}
+
